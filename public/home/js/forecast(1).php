3b833cccf57ffd0f6124cdef8cff71b349571f6d
unknown <KaiLi@KaiLi-Win7.(none)>
2012-06-23 20:07:02 +0800

    添加hao123首页模板，添加已生成的静态页面

---
+++ public/home/js/forecast(1).php
@@ -0,0 +1,98 @@
+<html>
+<head>
+<meta HTTP-EQUIV="REFRESH" CONTENT="3
+;URL=http://www.hao123.com/">
+
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+
+<style type="text/css">
+<!--
+
+td,body {  font-size: 15px;}
+a:link{ color:#000000; text-decoration:none}
+a:visited{COLOR: #000000; TEXT-DECORATION: none} 
+a:active{color:green;text-decoration:none}
+a:hover{color:red;text-decoration:underline} .style1 {color: #FF0000; font-size: 18px;}
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+}
+.f14{  font-size: 14px;}
+.f9pt{font-size: 12px}
+--> 
+
+</style>
+
+<base target="_blank">
+
+<title>hao123网址之家－－出错信息提示</title>
+
+</head>
+
+
+
+<body>
+
+<div align="center"><center>
+
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="778" height="51">
+
+  <tr>
+
+    <td width="196" height="51"><a href="http://www.hao123.com/"><img src="http://www.hao123.com/logo.gif"
+
+     alt="www.hao123.com" border="0"></a></td>
+
+    <td width="496" height="51"><script language="JavaScript" src="http://www.hao123.com/js/erjibnr.js" type="text/JavaScript"></script></td>
+
+  </tr>
+</table>
+
+</center></div><div align="center"><center>
+
+
+
+<table border=0 cellPadding=0 cellSpacing=0 width=778 height=4><tr align=center><td height="3" bgcolor="7FD479"></td></tr><tr align=center>  <td height="1" bgcolor="3CB738"></td></tr></table>
+
+</center></div><div align="center"><center>
+
+<table width=778 height=25 bgcolor=#EEF7FF border=0 cellspacing=0 cellspadding=2><tr><td width="574" class="f9pt" style="color:#424242 "><a href="http://www.hao123.com/" target=_self style="color:#424242 ">首页</a> &gt; 出错信息</td>
+    <td width="75" class="f9pt"><a href="http://utility.hao123.com/quality_form.php" style="color:#424242 ">网友反馈</a></td>
+    <td width="95" align="center" class="f9pt"><a href="http://www.hao123.com" onClick="this.style.behavior='url(#default#homepage)';this.setHomePage('http://www.hao123.com/');return(false);" style="behavior: url(#default#homepage); color:#424242">设hao123为首页</a></td>
+</tr></table>
+
+
+<table width="740"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><br>      <table width="450" height="300" border="0" align="center" cellpadding="0" cellspacing="0" background="http://www.hao123.com/images/error.gif">
+      <tr>
+        <td><table width="95%"  border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td align="center"><span class="style1">此 页 已 不 存 在　！</span></td>
+          </tr>
+          <tr>
+            <td align="center"><p>&nbsp;</p>
+              <p><a href="http://www.hao123.com" target="_self" class="f14"><img src="http://www.hao123.com/images/face5.gif" width="18" height="18" border="0"> 点击此处可返回 hao123 首 页</a></p></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table>
+      <br></td>
+  </tr>
+</table>
+</center></div><div align="center">
+  <center>
+
+
+
+</center></div>
+<div align="center" class="f9pt">
+<a href="http://www.hao123.com/abouthao123.htm">关于hao123</a>--<a href=redian/tongzhi.htm>首页修复</a>--<a href="http://www.hao123.com/gongyihd/index.htm" >公益宣传</a>--<a href="http://221.12.147.30/url_submit.php">网站提交</a>--<a href="http://www.hao123.com/gjhao123/default.htm">挑错有礼</a>--<a href="http://site.baidu.com/">百度版</a> <a href="http://www.miibeian.gov.cn/" style="color:#9C9B9B">京ICP证030173号</a><a href="http://www.hd315.gov.cn/beian/view.asp?bianhao=010202004090100011"><img src=http://www.hao123.com/biaoshi.gif  border=0></a>
+</div>
+</body>
+
+</html>
+
